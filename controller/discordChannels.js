b474ba3323367c219e82843cba59a7da20638abe
Patrick Pedersen <patricksoeholtpedersen@gmail.com>
2022-10-31 05:00:37 +0100

    Added Bot Channels controller
    Signed-off-by: Patrick Pedersen <patricksoeholtpedersen@gmail.com>

---
+++ controller/discordChannels.js
@@ -0,0 +1,21 @@
+class DiscordChannelsController {
+    async getAllVoiceChannels(client) {
+        const guild = await client.guilds.fetch(client.config.settings_guildId_dev2)
+        let voiceChannels = await guild.channels.cache.filter(channel => channel.type === 'GUILD_VOICE')
+
+        let formattedChannelInfo = []
+        for (let channel of voiceChannels) {
+            formattedChannelInfo.push({ id: channel[1].id, name: channel[1].name })
+        }
+
+        formattedChannelInfo.sort(function(a, b) {
+            let textA = a.name.toUpperCase();
+            let textB = b.name.toUpperCase();
+            return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+        })
+
+        return formattedChannelInfo
+    }
+}
+
+module.exports = DiscordChannelsController
