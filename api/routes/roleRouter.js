0929f0a28954e94c958b660ce26e9410df317173
Patrick Pedersen <patricksoeholtpedersen@gmail.com>
2022-10-31 04:35:52 +0100

    Added Role controller and router
    Signed-off-by: Patrick Pedersen <patricksoeholtpedersen@gmail.com>

---
+++ api/routes/roleRouter.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const roleController = require('../controllers/role')
+
+const router = express.Router()
+
+// bot/api/v?/role => POST
+router.post('/sync', roleController.sync)
+// router.post('/givebwc', roleController.givebwc)
+// router.post('/removebwc', roleController.removebwc)
+router.post('/give', roleController.give)
+router.post('/remove', roleController.remove)
+
+// bot/api/v?/role => GET
+router.get('/fetchallroles', roleController.fetchAllRoles)
+router.get('/forcesyncusers', roleController.forceSyncAllUsers)
+
+module.exports = router
