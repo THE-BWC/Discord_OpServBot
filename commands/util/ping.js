4c19515d1fa629bbe7e273ed69566ff6f9e07f67
Patrick Pedersen <patricksoeholtpedersen@gmail.com>
2024-07-10 23:41:38 +0200

    update ping command to work.

--- commands/util/ping.js
+++ commands/util/ping.js
@@ -1,6 +1,5 @@
-const { Client, CommandInteraction, MessageEmbed } = require('discord.js');
+const { Client, CommandInteraction, EmbedBuilder } = require('discord.js');
 const { SlashCommandBuilder } = require("@discordjs/builders");
-
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -8,7 +7,6 @@ module.exports = {
 		.setDescription('Replies with the latency of the bot'),
 
 	/**
-	 *
 	 * @param {Client} client
 	 * @param {CommandInteraction} interaction
 	 * @param {String[]} args
@@ -17,15 +15,16 @@ module.exports = {
 		let pingInitialization = Date.now()
 
 		interaction.deferReply()
-			.then(() => {
+			.then(async () => {
 				let ping = pingInitialization - interaction.createdTimestamp;
-				let pEmbed = new MessageEmbed()
+				let pEmbed = new EmbedBuilder()
 					.setColor([200,0,0])
 					.setTitle("Pong!")
 					.addField(`**Bot Latency:**`, `${ping / 1000}s`, true)
 					.addField(`**API Latency:**`, `${interaction.client.ws.ping / 1000}s`, true)
 					.addField(`**Bot Region:**`, client.config.server_location, true)
-				interaction.followUp({ embeds: [pEmbed] })
+				
+				await interaction.followUp({ embeds: [pEmbed] })
 			})
 	}
 }
